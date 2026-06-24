e93f6fd18f4517feae2743b0bb7b0951435ee987
schaefferchoi <99786331+schaefferchoi@users.noreply.github.com>
2026-06-25 08:38:24 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='tymict-install-customer-app-v2';const FILES=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
